a105d6dbaec2ba270fe5649f3a94b5a15debecb2
R Ito <co.panda.fc3s@gmail.com>
2018-06-03 22:48:29 +0900

    add problem 06-05

---
+++ ch06/prob05.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+
+def main():
+    strlist = ["The", "fox", "jumped", "over", "the", "fence", "."]
+    print(" ".join(strlist[:-1]) + strlist[-1])
+
+
+if __name__ == '__main__':
+    main()
