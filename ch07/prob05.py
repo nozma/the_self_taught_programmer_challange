ae2ebba2526378394af85dc499056e8f6c7ad5d9
R Ito <co.panda.fc3s@gmail.com>
2018-06-03 23:44:20 +0900

    add problem 07-05

---
+++ ch07/prob05.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+
+
+def main():
+    nums1 = [8, 19, 148, 4]
+    nums2 = [9, 1, 33, 83]
+    print(np.outer(nums1, nums2))
+
+
+if __name__ == '__main__':
+    main()
