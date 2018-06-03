8f7db902a1b76d19ad7eaefbb9d45c8a3a5c1500
R Ito <co.panda.fc3s@gmail.com>
2018-06-03 23:43:34 +0900

    add problem 07-03

---
+++ ch07/prob03.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+
+def main():
+    strlist = [
+        "ウォーキング・デッド",
+        "アントラージュ",
+        "ザ・ソプラノズ",
+        "ヴァンパイア・ダイアリーズ"
+    ]
+
+    for i, s in enumerate(strlist):
+        print(str(i), s)
+
+
+if __name__ == '__main__':
+    main()
