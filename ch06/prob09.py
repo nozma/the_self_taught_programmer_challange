7c927431ca72ab231fd10c5bffff516438dec3c2
R Ito <co.panda.fc3s@gmail.com>
2018-06-03 23:07:26 +0900

    add problem 06-09

---
+++ ch06/prob09.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+
+def main():
+    s = "three "
+    str1 = s + s + s
+    str2 = s * 3
+
+    print(str1.strip())
+    print(str2.strip())
+
+
+if __name__ == '__main__':
+    main()
