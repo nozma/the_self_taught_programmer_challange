8290560e2f8fed9952eb236d2331a2ac888fa1e0
R Ito <co.panda.fc3s@gmail.com>
2018-06-03 22:38:21 +0900

    add problem 06-02

---
+++ ch06/prob02.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+
+def main():
+    str1 = input("何を書きましたか?: ")
+    str2 = input("誰に送りましたか?: ")
+    print("私は昨日{}を書いて{}に送った!".format(str1, str2))
+
+
+if __name__ == '__main__':
+    main()
